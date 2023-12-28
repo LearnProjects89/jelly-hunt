df4d55a03ec4a7fbb8ccf657c2720ca49cf9de01
XenaSit <sitnikovaxena@gmail.com>
2023-12-28 16:25:56 -0500

    treasure hunt is working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import one from './assets/table/01.png';
 import two from './assets/table/02.png';
 import three from './assets/table/03.png';
@@ -16,73 +16,92 @@ import Github from  './assets/github.png'
 import './App.css';
 
 function App() {
-
-  const [winner, setWinner] = useState(treasure);
-  const [loser, setLoser] = useState(bomb);
-  const [wronger, setWronger] = useState(wrong);
+  const [results, setResults] = useState(Array(9).fill(null));
   const [counter, setCounter] = useState(5);
+  const [gameEnded, setGameEnded] = useState(false);
+  const [endGameResult, setEndGameResult] = useState('');
 
-  
-  const handleTreasure = (location) => {
-    setCounter(prevCounter => prevCounter - 1);
-    setWinner(Math.floor(Math.random() * 10));
-    setLoser(Math.floor(Math.random() * 10))
-    if (winner === loser) { loser = Math.floor(Math.random() * 10) }
 
-    if (location === winner) {
-      setWinner(winner);
-      alert("You Won!!!");
-    } else if (location === loser) {
-      setLoser(loser);
-      alert("Spongbob is annoyed! Please try again!");
-    } else {
-      setWronger(wronger);
+  useEffect(() => {
+    if (counter === 0) {
+      const randomBomb = Math.floor(Math.random() * 9);
+      const newResults = results.map((_, index) =>
+        index === randomBomb ? bomb : wrong
+      );
+      setResults(newResults);
+      setEndGameResult(bomb);
+      setGameEnded(true);
     }
+  }, [counter, results]);
+
+  const handleTreasure = (location) => {
+    setCounter((prevCounter) => prevCounter - 1);
+
+    const win = Math.floor(Math.random() * 9);
+    const lose = Math.floor(Math.random() * 9);
+
+    const newResults = [...results];
+
+    if (location === win) {
+      newResults[location] = treasure;
+      setEndGameResult(treasure);
+      setGameEnded(true);
+    } else if (location === lose) {
+      newResults[location] = bomb;
+      setEndGameResult(bomb);
+      setGameEnded(true);
+    } else {
+      newResults[location] = wrong;
+    }
+
+    setResults(newResults);
   };
 
   const handleRestartGame = () => {
     window.location.reload();
   };
-
+  
   return (
     
     <div className="App">
       <div className='container'>
         <h1>Help Spongy</h1> 
-        <h2>find jelly, avoid spongbob who asks quastions</h2>
+        <h2>You have 5 turns, find jelly, avoid spongbob who asks quastions!!!</h2>
         
         
         <h2 id="counter">TURNS: {counter}</h2>
 
+        {!gameEnded ? (
+          <div id="table" border="1">
+              <div className="table">
+                <div id="1" onClick={() => handleTreasure(1)}><img src={results[1] || one} alt="1" /></div>
+                <div id="2" onClick={() => handleTreasure(2)}><img src={results[2] || two} alt="2" /></div>
+                <div id="3" onClick={() => handleTreasure(3)}><img src={results[3] || three} alt="3" /></div>
+              </div>
+              <div className='table'>
+                <div id="4" onClick={() => handleTreasure(4)}><img src={results[4] || four} alt="4" /></div>
+                <div id="5" onClick={() => handleTreasure(5)}><img src={results[5] || five} alt="5" /></div>
+                <div id="6" onClick={() => handleTreasure(6)}><img src={results[6] || six} alt="6" /></div>
+              </div>
+              <div className='table'>
+                <div id="7" onClick={() => handleTreasure(7)}><img src={results[7] || seven} alt="7" /></div>
+                <div id="8" onClick={() => handleTreasure(8)}><img src={results[8] || eight} alt="8" /></div>
+                <div id="9" onClick={() => handleTreasure(9)}><img src={results[9] || nine} alt="9" /></div>
+              </div>
+          </div>
+        ) : (
+          <div className="end-game"><img src={endGameResult} alt="End Game Result" /></div>
+        )}
 
-        <div id="table" border="1">
-            <div className='table'>
-              <div id="1" onClick={() => handleTreasure(1)}><img src={one} alt="1"/></div>
-              <div id="2" onClick={() => handleTreasure(2)}><img src={two} alt="2"/></div>
-              <div id="3" onClick={() => handleTreasure(3)}><img src={three} alt="3"/></div>
-            </div>
-            <div className='table'>
-              <div id="4" onClick={() => handleTreasure(4)}><img src={four} alt="4"/></div>
-              <div id="5" onClick={() => handleTreasure(5)}><img src={five} alt="5"/></div>
-              <div id="6" onClick={() => handleTreasure(6)}><img src={six} alt="6"/></div>
-            </div>
-            <div className='table'>
-              <div id="7" onClick={() => handleTreasure(7)}><img src={seven} alt="7"/></div>
-              <div id="8" onClick={() => handleTreasure(8)}><img src={eight} alt="8"/></div>
-              <div id="9" onClick={() => handleTreasure(9)}><img src={nine} alt="9"/></div>
-            </div>
-        </div>
-        
-       
-        <button type="button" onClick={handleRestartGame}>Restart Game</button>
+        {gameEnded && (<button type="button" onClick={handleRestartGame}>Restart Game</button>)}
+
         <div className="social-links">
             <h1 style = {{color: 'lightgreen'}}>Checkout the REPO:</h1> 
             
-            <a href="https://github.com/XenaSit" target="_blank" rel="ln" className="custom-link">
+            <a href="https://github.com/XenaSit" target="_blank" rel="noreferrer" className="custom-link">
                 <img src={Github} alt="Github" className="custom-link" />
             </a>
         </div>
-
       </div>
     </div>
   );
